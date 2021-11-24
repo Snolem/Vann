8f61081983f8ce004a6cb2dccfc2d0373fdd0805
Elias <81151432+Elias-Chairi@users.noreply.github.com>
2021-11-24 15:21:50 +0100

    La til ett script som henter alle datene
    Dette er ett godt utganspunkt så slipper dere å styre med hvordan å hente dataene. 
    Prøvd å forklare så mye som mulig i koden slik at dere forstår den.

--- script.js
+++ script.js
@@ -23,19 +23,31 @@ function turbines(){
 
 
 // printer alt
+// ".then" venter på å motta en verdi før den fortsetter
+// der hvor jeg srkiver "value => {}" er en kortere version av "function(value) {}"
+// ordet value kan endres
+
+//denne er bare ett tall og kan printes direkte
 powerPrice().then(value => {
     console.log("powerPrice: " + value);
 });
+
+//denne er bare ett tall og kan printes direkte
 waterInflux().then(value => {
     console.log("waterInflux: " + value);
 });
+
+//denne er ett objekt hvor jeg henter environmentCost, money og waterLevel
 groupState().then(value => {
     console.log("environmentCost: " + value.environmentCost);
     console.log("money: " + value.money);
     console.log("waterLevel: " + value.waterLevel);
 });
+
+// denne er en liste med objekter
+// Så her velges først ett objekt fra listen, og så henter jeg capacityUsage
 turbines().then(value => {
     for (i = 0; i < value.length; i++){
-        console.log(`turbin ${i+1} capacityUsage: ${value[i].capacityUsage}`);
+        console.log("turbin " + (i+1) + " capacityUsage: " + value[i].capacityUsage);
     }
 });
