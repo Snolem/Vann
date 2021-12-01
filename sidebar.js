414e8ecc874606bf1bd6c9d4d9c4741ee1172807
Elias-Chairi <elcha002@osloskolen.no>
2021-12-01 11:28:52 +0100

    lagt til turbintab greie

--- sidebar.js
+++ sidebar.js
@@ -1,16 +1,16 @@
 let sidebar = document.getElementById("sidebar");
-let sidebarButton = document.getElementById("menuButton");
 let burgerDivs = document.querySelectorAll("#menuButton > div");
+
+let sidebarButton = document.getElementById("menuButton");
 sidebarButton.addEventListener("click", inOutSidebar);
 
 let inoutcounter = 1;
 function inOutSidebar() {
-    console.log("test");
     inoutcounter++;
     if (inoutcounter % 2 == 0){
         sidebar.style.width = "50px";
         for (let i = 0; i < burgerDivs.length; i++){
-            burgerDivs[i].style.width = "28px";
+            burgerDivs[i].style.width = "29px";
         }
     } else {
         sidebar.style.width = "250px";
