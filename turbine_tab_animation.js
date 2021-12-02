4b0eb856d6f0517063b6e70f4e1c1272ff081b36
Elias-Chairi <elcha002@osloskolen.no>
2021-12-02 16:31:16 +0100

    kan nå skrus av og på

--- turbine_tab_animation.js
+++ turbine_tab_animation.js
@@ -7,7 +7,38 @@ function turbines(){
     }).then(r =>r.json());
 }
 
+let turbineList = [];
+turbines().then(value => {
+    for (i = 0; i < value.length; i++){
+        turbineList.push(value[i].id)
+    }
+});
+
+function changeTurbineUsage(turbineIndex, usage){
+    fetch("https://innafjord.azurewebsites.net/api/Turbines/" + turbineList[turbineIndex] + "?capacityUsage=" + usage, {
+        method: "PUT",
+        headers: {
+            "GroupId": "Pavens vannkraftarbeidere",
+            "GroupKey": "/kJ+p7iy1kShleUuDqPNEA=="
+        }
+    });
+}
+
+function changeTurbineState(e){
+    const target = Array.from(turbiner).indexOf(e.target);
+    const target_src = turbiner[target].src;
+    if (target_src.indexOf("turbin_av") > -1){
+        changeTurbineUsage(target, 1);
+    } else {
+        changeTurbineUsage(target, 0);
+    }
+}
+
 const turbiner = document.querySelectorAll("#turbinpannel > div > img");
+for (let i = 0; i < turbiner.length; i++){
+    turbiner[i].addEventListener("click", changeTurbineState);
+}
+
 let rotation = 0;
 let turbineState = new Array(20).fill({'capacityUsage': 0}); // so i dont get undefined, when not having the values
 
